74d64852055de96caea29e3922be32a0116c62cc
Anthony <adominguez15722@gmail.com>
2022-02-14 16:21:24 -0600

    Initial Commit

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.static('public'))
 
 app.get('./', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/main.html'))
+    res.sendFile(path.join(__dirname, '..public/main.html'))
 })
 
 
