2ef51c25fada26e1c0887778045e13317ecbdec8
Anthony <adominguez15722@gmail.com>
2022-02-14 16:00:40 -0600

    Initial Commit

--- public/index.js
+++ public/index.js
@@ -1,22 +1,6 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const {shuffleCards, dealCards} = require('./controller')
-const app =express()
-const axios = require('axios')
+// const {shuffleCards, dealCards} = require('./controller')
 
-const path = require('path')
-
-axios
-app.use(cors())
-app.use(express.json())
-app.use(express.static('public'))
-
-app.get('./', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/main.html'))
-})
-app.get('./', shuffleCards)
-
-
-const port = process.env.PORT || process.env.SERVER_PORT
-app.listen(port, () => console.log(`Server serving on ${port}`))
+// app.get('./', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/main.html'))
+// })
+// app.get('./', shuffleCards)
