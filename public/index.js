5ef3f9837b40cd0dc2b128a1ddc7bbc5603584ac
Anthony <adominguez15722@gmail.com>
2022-02-14 16:14:42 -0600

    Initial Commit

--- public/index.js
+++ public/index.js
@@ -1,6 +1,4 @@
 // const {shuffleCards, dealCards} = require('./controller')
 
-// app.get('./', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/main.html'))
-// })
+
 // app.get('./', shuffleCards)
--- server/server.js
+++ server/server.js
@@ -11,7 +11,9 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
 
-
+app.get('./', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/main.html'))
+})
 
 
 const port = process.env.PORT || process.env.SERVER_PORT
