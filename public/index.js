15089279f32d7e76c45a6f6238ef37cb9962c947
Anthony <adominguez15722@gmail.com>
2022-02-14 15:36:56 -0600

    Initial Commit

--- public/index.js
+++ public/index.js
@@ -3,10 +3,11 @@ const express = require('express')
 const cors = require('cors')
 const {shuffleCards, dealCards} = require('./controller')
 const app =express()
+const axios = require('axios')
 
 const path = require('path')
 
-
+axios
 app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
